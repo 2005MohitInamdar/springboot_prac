3a84910532b5b499477bb24d173ad28b7b328e5b
2005MohitInamdar <1970mohitinamdar@gmail.com>
2026-01-09 10:28:54 +0530

    Made changes in HomeController and created package named model

--- src/main/java/com/example/prac/controller/HomeController.java
+++ src/main/java/com/example/prac/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.prac.controller;
 import org.springframework.web.bind.annotation.*;
+import com.example.prac.model.User;
+
 
 @RestController
 public class HomeController{
@@ -21,5 +23,10 @@ public class HomeController{
     @PostMapping("/greet")
     public String greet(@RequestParam String name){
         return "Hello" +name;
+    }
+
+    @PostMapping("/users")
+    public String user(@RequestBody User user){
+        return "Name is: " + user.name + ", Age is: " + user.age;
     }
 }
---
+++ src/main/java/com/example/prac/model/User.java
@@ -0,0 +1,6 @@
+package com.example.prac.model;
+
+public class User {
+    public String name;
+    public int age;
+}
